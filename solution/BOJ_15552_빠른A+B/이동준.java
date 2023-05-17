2354c834b969eb6679c3c070ac96bf9afe7ea8d3
GyulCode <ldg2057@naver.com>
2023-05-18 01:27:15 +0900

    112

---
+++ solution/BOJ_15552_빠른A+B/이동준.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.StringTokenizer;
+public class Main {
+	public static void main(String[] args) {
+		
+		BufferedReader br;
+		BufferedWriter bw;
+		
+		try
+		{
+			br=new BufferedReader(new InputStreamReader(System.in));
+			bw=new BufferedWriter(new OutputStreamWriter(System.out));
+			int totalline = Integer.parseInt(br.readLine());
+			StringTokenizer st;
+			
+			for(int i=0;i<totalline;i++) {
+				//StringTokenizer 두개의 구분문자도 가능 ">:" -> > 또는 : 만나면 새뢔운 토큰 생성
+				st = new StringTokenizer(br.readLine()," ");
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				//개행 오류 ^^;
+				bw.write((a+b)+"\n");				
+			}
+			br.close();
+			bw.flush();
+			bw.close();
+		}catch(Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+}
---
+++ solution/BOJ_2444_별찍기-7/이동준.java
@@ -0,0 +1,32 @@
+import java.io.*;
+public class Main {
+	public static void main(String[] args) {
+		
+		BufferedReader br;
+		try
+		{
+			br= new BufferedReader(new InputStreamReader(System.in));
+			int N=Integer.parseInt(br.readLine());
+			for(int i=1; i<=N;i++) {
+				for(int j=N-i;j>=1;j--) {
+					System.out.print(" ");
+				}
+				for(int x=1; x<=2*i-1; x++) {
+					System.out.print("*");
+				}
+				System.out.println();
+			}
+			for(int i=N-1; i>=1; i--) {
+				for(int j=1;j<=N-i; j++) {
+					System.out.print(" ");
+				}
+				for(int x=1;x<=2*i-1;x++) {
+					System.out.print("*");
+				}
+				System.out.println();
+			}
+		}catch(Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+}
