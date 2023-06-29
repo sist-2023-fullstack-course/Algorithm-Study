cf671bb98e5ee9c913e5793d7e08ae7dd635b4b7
tyuam96@gmail.com <tyuam96@gmail.com>
2023-06-30 01:22:02 +0900

    [BOJ]10798_나도원

---
+++ solution/BOJ_10798_세로읽기/나도원.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class BOJ_10798_세로읽기 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out=new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		char[][] arr=new char[5][15];
+		
+		for(int i=0;i<arr.length;i++) {
+			String word=in.readLine(); // 한 줄씩 읽어오기
+			
+			for(int j=0;j<word.length();j++) {
+				arr[i][j]=word.charAt(j); // 하나의 줄에서 한 글자씩 읽어와서 배열에 저장
+			}
+		}
+		
+		for(int i=0;i<15;i++) { // 최대 15글자를 읽고
+			for(int j=0;j<5;j++) { // 최대 5줄을 읽고
+				if(arr[j][i]==0) { // 값이 없는 경우 그대로 진행
+					continue;
+				}
+				out.write(arr[j][i]);
+				out.flush();
+			}
+		}
+		
+		
+	}
+
+}
+
